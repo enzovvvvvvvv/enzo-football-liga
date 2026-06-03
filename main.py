fd3e563aa234e114f0f146d3e07b05f69d745b13
enzovvvvvvvv <howtobestrong01@gmail.com>
2026-06-03 02:21:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 # Configuration
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+model = genai.GenerativeModel("gemini-1.5-flash-002")
 
 client = tweepy.Client(
     consumer_key=os.environ.get("X_API_KEY"),
